7ae03081a88f0c83e70ecd6cb52b16ce66e6de47
li <106793369+fooler64@users.noreply.github.com>
2023-04-11 16:42:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git small https://github.com/fooler64/small-package' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git v2rayA https://github.com/v2rayA/v2raya-openwrt' >>feeds.conf.default
